244480cc9c7872429a219db8d735b674d30eed8f
okadirkartal <161307071@kocaeli.edu.tr>
2019-11-26 05:35:04 +0300

    added new product model

---
+++ src/app/models/ProductAddViewModel.ts
@@ -0,0 +1,8 @@
+export class ProductAddViewModel {
+    sku:string;
+    name:string;
+    price:number;
+    unitTypeId:number;
+    unitValue:string;
+    productId:number;
+}
---
+++ src/app/models/ProductListViewModel.ts
@@ -0,0 +1,11 @@
+export interface ProductListViewModel {
+    sku:string;
+    name:string;
+    price:number;
+    unitTypeId:number;
+    size:number;
+    weight:number;
+    dimension_h:number;
+    dimension_w:number;
+    dimension_l:number;
+}
